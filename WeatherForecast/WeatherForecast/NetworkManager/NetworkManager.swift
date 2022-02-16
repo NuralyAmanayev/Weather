7472b86596878e9e04a990f58a33b8e235236421
Nuraly Amanayev <Nuraly Amanayev>
2022-02-17 02:11:06 +0300

    commit2

--- WeatherForecast/WeatherForecast/NetworkManager/NetworkManager.swift
+++ WeatherForecast/WeatherForecast/NetworkManager/NetworkManager.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+class NetworkManager {
+    func requestWeather(result: @escaping ([WeatherModel]) -> (), latitude: String, longitude: String){
+        let apiKey = "5308895e59599bce50322cbfd1f66036"
+        let url =  "https://api.openweathermap.org/data/2.5/forecast?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)"
+        URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
+
+            // validation
+            guard let data = data, error == nil else {
+                print("something went wrong")
+                return
+            }
+
+            // convert data to models/some object
+
+            var json: WeatherModel?
+            do{
+                json = try JSONDecoder().decode(WeatherModel.self, from: data)
+print(json)
+            }
+            
+            
+            catch{
+                print("error: \(error)")
+            }
+
+            guard let result = json else{
+                return
+            }
+
+            
+        
+        }).resume()
+
+        
+    }
+}
--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate{
+    var manager: CLLocationManager?
+    var networkManager = NetworkManager()
+    var weatherModel = [WeatherModel]()
+    
+    
+    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        manager = CLLocationManager()
+        manager?.delegate = self
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        manager?.requestWhenInUseAuthorization()
+        manager?.startUpdatingLocation()
+    }
 
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let currentLocation = locations.first else {
+            return
+        }
+        print("\(currentLocation.coordinate.longitude)|\(currentLocation.coordinate.latitude)")
+//        self.long = currentLocation.coordinate.longitude
+//        var lat = first.coordinate.latitude
+        manager.stopUpdatingLocation()
+    }
+    
 }
 
