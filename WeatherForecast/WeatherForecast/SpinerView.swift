37c34ee6ae8360befcfc9dbeecc9cebb383f6d31
Nuraly Amanayev <Nuraly Amanayev>
2022-02-18 15:03:23 +0300

    commit6

---
+++ WeatherForecast/WeatherForecast/SpinerView.swift
@@ -0,0 +1,45 @@
+//
+//  SpinerView.swift
+//  WeatherForecast
+//
+//  Created by Nuraly Amanayev on 2/18/22.
+//
+
+import UIKit
+
+class SpinerView: UIView {
+
+    var activityIndicatorView: UIActivityIndicatorView = UIActivityIndicatorView()
+        static let defaultHeight:CGFloat = 60.0
+        
+        required init?(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
+            setupActivityIndicator()
+        }
+        
+        override init(frame aRect: CGRect) {
+            super.init(frame: aRect)
+            setupActivityIndicator()
+        }
+        
+        override func layoutSubviews() {
+            super.layoutSubviews()
+            activityIndicatorView.center = CGPoint(x: self.bounds.size.width/2, y: self.bounds.size.height/2)
+        }
+        
+        func setupActivityIndicator() {
+            activityIndicatorView.style = .medium
+            activityIndicatorView.hidesWhenStopped = true
+            self.addSubview(activityIndicatorView)
+        }
+        
+        func stopAnimating() {
+            self.activityIndicatorView.stopAnimating()
+            self.isHidden = true
+        }
+        
+        func startAnimating() {
+            self.isHidden = false
+            self.activityIndicatorView.startAnimating()
+        }
+    }
--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController{//, CLLocationManagerDelegate{
     var networkManager = NetworkManager()
     var weatherModel = [WeatherModel]()
     var getLocation = GetLocation()
+    var isMoreDataLoading = false
+    var indicator = 0
+    @IBOutlet weak var CustomTableView: UITableView!
+    var loadingMoreView:SpinerView?
 //    var list = weatherMo
     
     
@@ -20,12 +24,13 @@ class ViewController: UIViewController{//, CLLocationManagerDelegate{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        networkManager.requestWeather { data in
-            DispatchQueue.main.async { [self] in
-                           self.weatherModel = data
-            }}
-        
-        print(weatherModel)
+        loadingData()
+//        networkManager.requestWeather { data in
+//            DispatchQueue.main.async { [self] in
+//                           self.weatherModel = data
+//            }}
+//
+//        print(weatherModel)
         // Do any additional setup after loading the view.
     }
     
@@ -34,6 +39,30 @@ class ViewController: UIViewController{//, CLLocationManagerDelegate{
         getLocation.setupLocation()
     }
 
+    
+    func loadingData(){
+        networkManager.requestWeather { data in
+            DispatchQueue.main.async {
+                [self] in
+                self.weatherModel = data
+                self.CustomTableView.reloadData()
+                
+                Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false){ timer in
+                    self.isMoreDataLoading = false
+                    self.loadingMoreView?.stopAnimating()
+                    if self.indicator == 1{
+                        self.CustomTableView.contentInset.top -= SpinerView.defaultHeight
+                    } else if self.indicator == 2 {
+                        self.CustomTableView.contentInset.bottom -= SpinerView.defaultHeight
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    
+    
 
 }
 
