1814ebeaeab7dbc6fd1546df32d1adce4350a831
Nuraly Amanayev <Nuraly Amanayev>
2022-02-20 01:34:16 +0300

    commit7

--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -16,7 +16,8 @@ class ViewController: UIViewController{//, CLLocationManagerDelegate{
     var isMoreDataLoading = false
     var indicator = 0
     @IBOutlet weak var CustomTableView: UITableView!
-    var loadingMoreView:SpinerView?
+//    var loadingMoreView:SpinerView?
+    var loadingMoreView = SpinerView()
 //    var list = weatherMo
     
     
@@ -41,15 +42,26 @@ class ViewController: UIViewController{//, CLLocationManagerDelegate{
 
     
     func loadingData(){
+        
+//        loadingMoreView.frame = CGRect(x: 30, y: 30, width: 100, height: 100)
+        loadingMoreView.translatesAutoresizingMaskIntoConstraints = false
+        loadingMoreView.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        loadingMoreView.heightAnchor.constraint(equalToConstant: 80).isActive = true
+                view.addSubview(loadingMoreView)
+        loadingMoreView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        loadingMoreView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+
         networkManager.requestWeather { data in
+            
             DispatchQueue.main.async {
-                [self] in
+//                [self] in
+                self.loadingMoreView.startAnimating()
                 self.weatherModel = data
                 self.CustomTableView.reloadData()
                 
                 Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false){ timer in
                     self.isMoreDataLoading = false
-                    self.loadingMoreView?.stopAnimating()
+                    self.loadingMoreView.stopAnimating()
                     if self.indicator == 1{
                         self.CustomTableView.contentInset.top -= SpinerView.defaultHeight
                     } else if self.indicator == 2 {
